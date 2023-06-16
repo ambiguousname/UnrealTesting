f13727fa423ad9e4c073a797cc65ad608071edf1
Tyler K <tyly04@gmail.com>
2023-06-16 15:20:13 -0700

    Initial commit

---
+++ Source/UnrealTest/UnrealTestGameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealTestGameMode.h"
+#include "UnrealTestCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AUnrealTestGameMode::AUnrealTestGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
